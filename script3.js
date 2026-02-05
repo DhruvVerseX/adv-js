c078889f8c8121ed6996704521226fd384912c87
DhruvVerseX <dhruvnegi2040@gmail.com>
2026-02-05 21:24:07 +0530

    feat: Add `script3.js` demonstrating prototype inheritance and update `index.html` to load it.

---
+++ script3.js
@@ -0,0 +1,17 @@
+//Ptotype Inheritance
+
+const k1 = {
+  name: "Dhruv",
+  age: 23,
+  __proto__: k3,
+};
+
+const k2 = {
+  name: "goes",
+  age: 23,
+};
+
+const k3 = {
+  name: "hell",
+  age: 23,
+};
